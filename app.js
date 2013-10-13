e0cb1ff0a07ba4010f059f89ba17a9a5f17c6a60
Bui Phi Quan <rongxanh2052004@yahoo.com>
2013-10-13 10:10:17 +0700

    fix bugs

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
+  app.use(express.static(path.join(__dirname, '/public')));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   // app.use(express.cookieParser('your secret here'));
   // app.use(express.session());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, '/public')));
 });
 
 app.configure('development', function(){
