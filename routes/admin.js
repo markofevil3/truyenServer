01217345b5d414f34c038893b48ff9f39486b5e5
Bui Phi Quan <rongxanh2052004@yahoo.com>
2013-10-23 22:27:16 +0700

    fix bugs

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@
 var Util = require('../lib/util');
 var email   = require('emailjs/email');
 var request = require('request');
+var adminRoute = require('./admin');
 
 var User = require('../models/models').User;
 var Chapter = require('../models/models').Chapter;
@@ -11,7 +12,7 @@ var Manga = require('../models/models').Manga;
 var Story = require('../models/models').Story;
 
 exports.index = function(req, res) {
-  Story.find({}, '_id title author datePost numView shortDes type chapters.chapter').sort( 'title', 1 ).exec(function(error, stories) {
+  Story.find({}, '_id title author datePost numView shortDes type').sort( 'title', 1 ).exec(function(error, stories) {
     if (error) {
       console.log(error);
     }
@@ -22,3 +23,26 @@ exports.index = function(req, res) {
     });
   });
 };
+
+exports.updateStories = function(req, res) {
+  Story.find({}, '_id title author shortDes type').sort( 'title', 1 ).exec(function(error, stories) {
+    if (error) {
+      console.log(error);
+    }
+    var count = 0;
+    for (var i = 0; i < stories.length; i++) {
+      if (stories[i].title != req.body["story-title-" + stories[i]._id.toString()]) {
+        stories[i].title = req.body["story-title-" + stories[i]._id.toString()];
+      }
+      if (stories[i].author != req.body["story-author-" + stories[i]._id.toString()]) {
+        stories[i].author = req.body["story-author-" + stories[i]._id.toString()];
+      }
+      stories[i].save(function() {
+        count++;
+        if (count == stories.length) {
+          adminRoute.index(req, res);
+        }
+      })
+    }
+  });
+}
